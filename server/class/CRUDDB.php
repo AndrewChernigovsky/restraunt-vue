6c3c0d1b52c84866b7e9056a9ea40b225da7951b
Izosimov_Andrew <chernigovsky108@gmail.com>
2024-09-24 16:39:37 +0300

    asa

--- server/class/CRUDDB.php
+++ server/class/CRUDDB.php
@@ -12,7 +12,7 @@ class CRUDDB extends ConnectDB
   {
     $tableCreationQuery = "
     CREATE TABLE IF NOT EXISTS $tableName (
-      id  VARCHAR(255) NOT NULL PRIMARY KEY,
+      id INT AUTO_INCREMENT PRIMARY KEY,
       name VARCHAR(255) NOT NULL,
       path VARCHAR(255) NOT NULL
     )";
